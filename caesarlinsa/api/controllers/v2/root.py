98bd9beec04c5a43971611c679022f5d3fee816b
caesar <Caesar_Linsa@163.com>
2018-08-12 02:51:22 +0800

    caesar-api add query api

--- caesarlinsa/api/controllers/v2/root.py
+++ caesarlinsa/api/controllers/v2/root.py
@@ -8,7 +8,5 @@ class V2Controller(rest.RestController):
 
     @pecan.expose()
     def _lookup(self, kind, *remainder):
-        if kind in ['meters', 'resources', 'samples']:
-            if kind == 'meters' and pecan.request.method == 'POST':
-                return meters.MetersController(), remainder
-
+        if kind == 'meters':
+            return meters.MetersController(), remainder
