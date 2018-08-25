2258209e9e7a35e59ef9ed0e68427c5d1913ee82
caesar <Caesar_Linsa@163.com>
2018-08-25 17:17:30 +0800

    update error after install and test

--- caesarlinsa/python_caesarlinsa/v2/meters.py
+++ caesarlinsa/python_caesarlinsa/v2/meters.py
@@ -1,8 +1,12 @@
+import requests
 
 from caesarlinsa.python_caesarlinsa.util import args
 
 @args('-m', '--meter', metavar='<METER>', help="help for find sample with meter_name")
 @args('-l', '--limit', metavar='<NUMBER>', help='help for find samples number')
 def do_sample_list(args):
-    print("meter:%s limit:%s" %(args.meter, args.limit))
+    url='http://localhost:8777/v2/meters/%s?limit=%s' %(args.meter,args.limit)
+    response = requests.get(url)
+    print response.json()
+
 
